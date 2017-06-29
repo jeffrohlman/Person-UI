c8eb97d5d4e737c89aaf7cd49a3e0b57f7ca7509
Jeffrey Rohlman <jeffreyrohlman@jeffreys-mbp.attlocal.net>
2017-06-29 17:08:27 -0500

    Initial build commit

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Jeffrey Rohlman on 6/29/17.
+//  Copyright © 2017 Jeffrey Rohlman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
